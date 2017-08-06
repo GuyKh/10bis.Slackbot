2152378732a151ce29f495e4d67a13f4389cc7a9
Guy Khmel <guykhmel@gmail.com>
2017-08-06 21:40:05 +0300

    Fix last merge

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
-<<<<<<< HEAD
 var botApp = require('./src/app.js');
-=======
-var botApp = require('./app.js');
->>>>>>> 763ebeef68fb4e142d3a50f48c5527d9a6e3bdb9
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -16,13 +13,8 @@ app.get('/', function(req, res) {
     res.send('Sanity passed!');
 });
 
-<<<<<<< HEAD
 app.post('/post', function(req, res) {
     botApp.process(req, res);
-=======
-app.post('/post', function(req, res){
-  botApp.process(req, res);
->>>>>>> 763ebeef68fb4e142d3a50f48c5527d9a6e3bdb9
 });
 
 app.listen(app.get('port'), function() {
