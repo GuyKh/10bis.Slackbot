e85511ede09604dd3b6134b1475286dead13a7fd
Guy Khmelnitsky <guykhmel@gmail.com>
2020-09-03 20:10:15 +0300

    Snyk insecure packages cleanup (#354)
    * Snyk insecure packages cleanup
    
    * Missing files
    
    * Add Husky pre-push lint
    
    Co-authored-by: Guy Khmelnitsky <guyk@oath.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,32 @@
 module.exports = {
-   "env": {
-      "node": true,
-      "mocha": true,
-      "es6": true
-   },
-   "extends": [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "prettier/@typescript-eslint",
-      "plugin:prettier/recommended"
-   ],
-   "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
-   },
-   "parser": "@typescript-eslint/parser",
-   "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
-   },
-   "plugins": [
-      "mocha",
-      "chai-expect",
-      "@typescript-eslint"
-   ],
-   "rules": {
-
-   },
-   "overrides": [
-      {
-        "files": ['*.ts', '*.tsx'],
-        "rules": {
-          '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
-        }
-      }
-    ]
+  env: {
+    node: true,
+    mocha: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["mocha", "chai-expect", "@typescript-eslint"],
+  rules: {},
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [2, { args: "none" }],
+      },
+    },
+  ],
 };
