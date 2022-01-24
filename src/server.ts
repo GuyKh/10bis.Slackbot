1ae7a7ad0ba1f6bd5af73690cae910b032805127
Guy Khmelnitsky <guykhmel@gmail.com>
2022-01-24 12:28:26 +0000

    Add RateLimiter to Express App

--- src/server.ts
+++ src/server.ts
@@ -63,6 +63,16 @@ export class Server {
    * @method config
    */
   public config() {
+    // set up rate limiter: maximum of five requests per minute
+    var RateLimit = require('express-rate-limit');
+    var limiter = new RateLimit({
+      windowMs: 1*60*1000, // 1 minute
+      max: 5
+    });
+
+    // apply rate limiter to all requests
+    this.app.use(limiter);
+
     // mount json form parser
     this.app.use(bodyParser.json());
 
